98dee0fc926a5b2a5e26399f7d7c8662a7b234a1
Jack <jack@agilitee.com>
2025-08-19 08:47:31 -0400

    Initial Commit

---
+++ pokesearch/pokesearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokesearchApp.swift
+//  pokesearch
+//
+//  Created by Jack Nichols on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct pokesearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pokesearchTests/pokesearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokesearchTests.swift
+//  pokesearchTests
+//
+//  Created by Jack Nichols on 8/19/25.
+//
+
+import Testing
+@testable import pokesearch
+
+struct pokesearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
